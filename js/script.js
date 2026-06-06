7b31c0bbd668997023ab6802d1307de7b5ad8de6
Khode_313 <mamalijafari41@gmail.com>
2026-06-06 23:39:28 +0430

    Refactor clock update function and interval setup

--- js/script.js
+++ js/script.js
@@ -2,31 +2,20 @@ function updateClock(){
 
 const now = new Date();
 
-let sec = now.getSeconds() * 6;
-let min = now.getMinutes() * 6;
-let hr = now.getHours() * 30 + now.getMinutes()/2;
+const sec = now.getSeconds() * 6;
+const min = now.getMinutes() * 6;
+const hr = now.getHours() * 30 + now.getMinutes()/2;
 
 document.getElementById("second").style.transform =
-`translateX(-50%) rotate(${sec}deg)`;
+`translate(-50%, -100%) rotate(${sec}deg)`;
 
 document.getElementById("minute").style.transform =
-`translateX(-50%) rotate(${min}deg)`;
+`translate(-50%, -100%) rotate(${min}deg)`;
 
 document.getElementById("hour").style.transform =
-`translateX(-50%) rotate(${hr}deg)`;
+`translate(-50%, -100%) rotate(${hr}deg)`;
 
 }
 
-/* gears move every second */
-setInterval(()=>{
-updateClock();
-
-document.querySelectorAll(".gear").forEach(g=>{
-    let r = (parseInt(g.dataset?.r || 0) + 6);
-    g.dataset.r = r;
-    g.style.transform = `rotate(${r}deg)`;
-});
-
-},1000);
-
+setInterval(updateClock,1000);
 updateClock();
